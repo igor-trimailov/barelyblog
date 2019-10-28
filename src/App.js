30ff2f16ab9d69c691a1ed3ca89f5d8810990240
Igor Trimailov <igor@Igors-MacBook-Pro.local>
2019-10-28 22:13:25 +0000

    fixed data loading

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import { useFetch } from './hooks'
 
 function App() {
   const [data, loading] = useFetch(
-    'https://stark-brushlands-58685.herokuapp.com/users/show/'
+    'https://stark-brushlands-58685.herokuapp.com/users/show/',
   )
 
-  /*eslint-disable-next-line */
-  console.warn(data)
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (!data) {
+    return <div>No data</div>
+  }
 
   return (
     <div className="App">
-      {loading ? (
-        'Loading...'
-      ) : (
-        <div>
-          <div>Status: {data.status}</div>
-          <div>Message: {data.message}</div>
-          <ul>
-            {data.users.map(item => (
-              <li key={item.name}>{item.name}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <div>Status: {data.status}</div>
+      <div>Message: {data.message}</div>
+      <ul>
+        {data.users.map(item => (
+          <li key={item.name}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
